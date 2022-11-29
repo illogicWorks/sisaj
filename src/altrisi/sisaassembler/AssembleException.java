b0a6e59b0ceb12b471dfe67ed279c18c5c47240f
altrisi <altrisi.trillosierra@gmail.com>
2022-11-29 14:45:07 +0100

    Document `AssembleException`

--- src/altrisi/sisaassembler/AssembleException.java
+++ src/altrisi/sisaassembler/AssembleException.java
@@ -1,12 +1,22 @@
 package altrisi.sisaassembler;
 
+/**
+ * An {@link AssembleException} indicates the cause of a failure in the assembly of an instruction
+ * in an {@link Assembler}, as its {@link #getMessage() detail message}. It may also contain a {@link #getCause() cause},
+ * though it's not required (it may be {@code null}).<p>
+ * 
+ * It will be passed to its {@link Assembler#failedLine(String, int, AssembleException)} method.<p>
+ * 
+ * @author altrisi
+ *
+ */
 @SuppressWarnings("serial")
 public class AssembleException extends Exception {
-	public AssembleException(String msg) {
+	AssembleException(String msg) {
 		super(msg);
 	}
 
-	public AssembleException(String msg, Throwable cause) {
+	AssembleException(String msg, Throwable cause) {
 		super(msg, cause);
 	}
 
